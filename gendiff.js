63c35579b52f26148878f9fcf543beecfd70da31
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-01-20 22:29:42 +0300

    add arguments in gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -14,6 +14,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.0')
+  .arguments('[filePath1> <filePath2]')
   .option('-f, --format [type]', 'output format')
   .action(() => {
     const options = program.opts();
