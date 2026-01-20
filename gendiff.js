df7a3e67218e2c89ad21767578f4bffc1dbfc0de
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-01-20 22:04:01 +0300

    add format function

--- gendiff.js
+++ gendiff.js
@@ -2,9 +2,22 @@ import { Command } from 'commander';
 
 const program = new Command();
 
+const changeFormat = (type) => {
+  if (type === 'format') {
+    console.log(`now is type: ${type}`);
+  } else {
+    console.log(`Format selected: ${type}`);
+  }
+};
+
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.0');
+  .version('0.0.0')
+  .option('-f, --format [type]', 'output format')
+  .action(() => {
+    const options = program.opts();
+    changeFormat(options.format);
+  });
 
 program.parse(process.argv);
