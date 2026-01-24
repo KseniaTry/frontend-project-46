d4283b1b97a4d36d0310a581f9b27ac313777105
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-01-24 22:26:00 +0300

    add parse.js

--- parse.js
+++ parse.js
@@ -1,5 +1,31 @@
 import { readFileSync } from 'fs';
+import { cwd } from 'process';
+import YAML from 'yaml'
 
-const parseData = filePath => console.log(readFileSync((filePath), 'utf8'))
+const normalizeFilePath = path => path.charAt(0) === '/' ? `${cwd()}${path}` : path
+
+const getFileType = path => path.toLowerCase().trim().split('.').at(-1)
+
+const parseData = (filePath) => {
+    const normalizedPath = normalizeFilePath(filePath)
+    const fileType = getFileType(filePath)
+    const data = readFileSync((normalizedPath), 'utf8')
+    console.log(fileType)
+    let parsedData
+
+    switch (fileType) {
+        case 'yml':
+            parsedData = YAML.parse(data)
+            break
+        case 'yaml':
+            parsedData = YAML.parse(data)
+            break
+        case 'json':
+            parsedData = JSON.parse(data)
+            break
+    }
+    console.log(parsedData)
+    return parseData
+}
 
 export default parseData
