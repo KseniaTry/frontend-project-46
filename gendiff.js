bca4758269520492c88c41e802fa0644583697a6
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-02-13 14:45:20 +0300

    add default format in gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -17,11 +17,11 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.0')
   .arguments('[filePath1> <filePath2]')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filePath1, filePath2) => {
     const options = program.opts();
     changeFormat(options.format);
-    const diff = genDiff(filePath1, filePath2, 'stylish')
+    const diff = genDiff(filePath1, filePath2, options.format)
     console.log(diff)
   });
 
