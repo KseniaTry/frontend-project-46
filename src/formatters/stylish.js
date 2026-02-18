52edb5a6d6d0e016c5880a705da9644a05e46dbd
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-02-18 14:49:30 +0300

    delete comments from stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -5,28 +5,28 @@ const SIGNES = {
     'changed': ' ',
 }
 
-const COMMENTS = {
-    oldValue: ' # Старое значение',
-    newValue: ' # Новое значение',
-    added: ' # Добавлена',
-    deleted: ' # Удалена',
-    empty: '# значения нет, но пробел после : есть',
-}
+// const COMMENTS = {
+//     oldValue: ' # Старое значение',
+//     newValue: ' # Новое значение',
+//     added: ' # Добавлена',
+//     deleted: ' # Удалена',
+//     empty: '# значения нет, но пробел после : есть',
+// }
 
-const getComment = (sign) => {
-    let comment
-    switch (sign) {
-        case '+':
-            comment = COMMENTS.added
-            break
-        case '-':
-            comment = COMMENTS.deleted
-            break
-        default:
-            comment = ''
-    }
-    return comment
-}
+// const getComment = (sign) => {
+//     let comment
+//     switch (sign) {
+//         case '+':
+//             comment = COMMENTS.added
+//             break
+//         case '-':
+//             comment = COMMENTS.deleted
+//             break
+//         default:
+//             comment = ''
+//     }
+//     return comment
+// }
 
 function formatDiffInStylish(data) {
     const iter = (data, depth) => {
@@ -46,31 +46,31 @@ function formatDiffInStylish(data) {
 
         for (const { key, status, oldValue, newValue, children } of data) {
             const sign = SIGNES[status]
-            let comment = getComment(sign)
-            if (newValue === " ") {
-                comment = COMMENTS.empty
-            }
+            // let comment = getComment(sign)
+            // if (newValue === " ") {
+            //     comment = COMMENTS.empty
+            // }
 
             if ((status === 'changed') && (children)) {
-                lines.push(`${indent}${sign} ${key}: {${comment}`);
+                lines.push(`${indent}${sign} ${key}: {`);
                 lines.push(...iter(children, depth + 4))
                 lines.push(`${innerIndent}}`);
             } else if (oldValue) {
                 if (Array.isArray(oldValue)) {
-                    lines.push(`${indent}- ${key}: {${COMMENTS.oldValue}`);
+                    lines.push(`${indent}- ${key}: {`);
                     lines.push(...iter(oldValue, depth + 4))
                     lines.push(`${innerIndent}}`);
-                    newValue === " " ? lines.push(`${indent}+ ${key}: ${newValue}${COMMENTS.empty}`) : lines.push(`${indent}+ ${key}: ${newValue}${COMMENTS.newValue}`);
+                    newValue === " " ? lines.push(`${indent}+ ${key}: ${newValue}`) : lines.push(`${indent}+ ${key}: ${newValue}`);
                 } else {
-                    oldValue === " " ? lines.push(`${indent}- ${key}: ${oldValue}${COMMENTS.empty}`) : lines.push(`${indent}- ${key}: ${oldValue}${COMMENTS.oldValue}`);
-                    newValue === " " ? lines.push(`${indent}+ ${key}: ${newValue}${COMMENTS.empty}`) : lines.push(`${indent}+ ${key}: ${newValue}${COMMENTS.newValue}`);
+                    oldValue === " " ? lines.push(`${indent}- ${key}: ${oldValue}`) : lines.push(`${indent}- ${key}: ${oldValue}`);
+                    newValue === " " ? lines.push(`${indent}+ ${key}: ${newValue}`) : lines.push(`${indent}+ ${key}: ${newValue}`);
                 }
             } else if (!oldValue && Array.isArray(newValue)) {
-                lines.push(`${indent}${sign} ${key}: {${comment}`);
+                lines.push(`${indent}${sign} ${key}: {`);
                 lines.push(...iter(newValue, depth + 4))
                 lines.push(`${innerIndent}}`);
             } else {
-                lines.push(`${indent}${sign} ${key}: ${newValue}${comment}`);
+                lines.push(`${indent}${sign} ${key}: ${newValue}`);
             }
         }
 
