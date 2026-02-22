22fb292d3a9a8bc11b997d598d25a818613104bd
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2026-02-22 14:38:25 +0300

    add stylistic in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
+  stylistic.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.jest,
         ...globals.node,
-      }
+      },
     },
   },
-]);
+])
